dba2bcceb51ed547f160b05fdceebef7f6ce1ce8
wako88 <hachiii88@gmail.com>
2026-07-15 14:44:11 +0900

    Part6 print.js 完成

---
+++ print.js
@@ -0,0 +1,14 @@
+/* ===========================
+   Favot Shift Ver1.0
+   print.js
+=========================== */
+
+const printBtn = document.getElementById("printBtn");
+
+printBtn.addEventListener("click", printShift);
+
+function printShift() {
+
+    window.print();
+
+}
